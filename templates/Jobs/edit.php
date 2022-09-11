2b8cfa868c319126ab67092fcf5d62e013a45fe1
Zhenhao <zzhu0019@student.monash.edu>
2022-09-12 03:26:04 +0800

    debug10

--- templates/Jobs/edit.php
+++ templates/Jobs/edit.php
@@ -4,6 +4,33 @@
  * @var \App\Model\Entity\Job $job
  */
 ?>
+
+
+<div class="row">
+    <div class="column-responsive column-80">
+        <div class="modal-dialog">
+            <div class = "modal-content">
+                <div class = "modal-header">
+                    <legend><?= __('Edit Job') ?></legend>
+                </div>
+                <div class = "modal-body">
+                    <?= $this->Form->create($job,['type'=>'file']) ?>
+                    <fieldset>
+                        <?php
+                        echo $this->Form->control('job_desc');
+                        echo $this->Form->control('job_price');
+                        echo $this->Form->control('job_time');
+                        ?>
+                    </fieldset>
+                </div>
+                <div class = "modal-footer">
+                    <?= $this->Form->button(__('Submit'),['class'=>'d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm']) ?>
+                    <?= $this->Form->end() ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="row">
     <aside class="column">
         <div class="side-nav">
@@ -11,24 +38,14 @@
             <?= $this->Form->postLink(
                 __('Delete'),
                 ['action' => 'delete', $job->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $job->id), 'class' => 'side-nav-item']
+                ['confirm' => __('Are you sure you want to delete # {0}?', $job->id), 'class'=>'d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm']
             ) ?>
-            <?= $this->Html->link(__('List Jobs'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
+
         </div>
+        <?= $this->Html->link(__('List Jobs'), ['action' => 'index'], ['class' => 'd-none d-sm-inline-block btn btn-sm btn-primary shadow-sm']) ?>
     </aside>
-    <div class="column-responsive column-80">
-        <div class="jobs form content">
-            <?= $this->Form->create($job) ?>
-            <fieldset>
-                <legend><?= __('Edit Job') ?></legend>
-                <?php
-                    echo $this->Form->control('job_desc');
-                    echo $this->Form->control('job_price');
-                    echo $this->Form->control('job_time');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
-        </div>
-    </div>
+
+
 </div>
+
+
--- templates/Quotes/edit.php
+++ templates/Quotes/edit.php
@@ -32,3 +32,51 @@
         </div>
     </div>
 </div>
+
+
+
+
+<div class="row">
+    <div class="column-responsive column-80">
+        <div class="modal-dialog">
+            <div class = "modal-content">
+                <div class = "modal-header">
+                    <legend><?= __('Edit Quote') ?></legend>
+                </div>
+                <div class = "modal-body">
+                    <?= $this->Form->create($quote,['type'=>'file']) ?>
+                    <fieldset>
+                        <?php
+                        echo $this->Form->control('quote_amount');
+                        echo $this->Form->control('order_id', ['options' => $orders]);
+                        ?>
+                    </fieldset>
+                </div>
+                <div class = "modal-footer">
+                    <?= $this->Form->button(__('Submit'),['class'=>'d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm']) ?>
+                    <?= $this->Form->end() ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <aside class="column">
+        <div class="side-nav">
+            <h4 class="heading"><?= __('Actions') ?></h4>
+            <?= $this->Form->postLink(
+                __('Delete'),
+                ['action' => 'delete', $quote->id],
+                ['confirm' => __('Are you sure you want to delete # {0}?', $quote->id), 'class'=>'d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm']
+            ) ?>
+
+        </div>
+        <?= $this->Html->link(__('List Quotes'), ['action' => 'index'], ['class' => 'd-none d-sm-inline-block btn btn-sm btn-primary shadow-sm']) ?>
+    </aside>
+
+
+</div>
+
+
+
+
