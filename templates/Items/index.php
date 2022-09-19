a3df2d5dfb34e61060db6204e13bd5f762bf9ff1
Zhenhao <zzhu0019@student.monash.edu>
2022-09-19 13:51:31 +0800

    debug 10

--- templates/Items/index.php
+++ templates/Items/index.php
@@ -3,26 +3,38 @@
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Item[]|\Cake\Collection\CollectionInterface $items
  */
+echo $this->Html->css('/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet', ['block' => true]);
+echo $this->Html->script('/vendor/datatables/jquery.dataTables.min.js', ['block' => true]);
+echo $this->Html->script('/vendor/datatables/dataTables.bootstrap4.min.js', ['block' => true]);
 ?>
-<div class="items index content">
-    <?= $this->Html->link(__('New Item'), ['action' => 'add'], ['class' => 'button float-right']) ?>
-    <h3><?= __('Items') ?></h3>
+
+
+
+
+<div class="orders index content">
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800"><?= __('Items') ?></h1>
+        <a href="<?= $this->Url->build(['action' => 'add']) ?>" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                class="fas fa-solid fa-plus fa-sm text-white-50"></i> New Order</a>
+    </div>
+
+
     <div class="table-responsive">
-        <table>
+        <table class="table table-bordered" id="dataTable" width="100%">
             <thead>
-                <tr>
-                    <th><?= $this->Paginator->sort('id') ?></th>
-                    <th><?= $this->Paginator->sort('name') ?></th>
-                    <th><?= $this->Paginator->sort('item_quantity') ?></th>
-                    <th><?= $this->Paginator->sort('item_price') ?></th>
-                    <th><?= $this->Paginator->sort('quantity_threshold') ?></th>
-                    <th><?= $this->Paginator->sort('description') ?></th>
-                    <th><?= $this->Paginator->sort('category_id') ?></th>
-                    <th class="actions"><?= __('Actions') ?></th>
-                </tr>
+            <tr>
+                <th><?= $this->Paginator->sort('id') ?></th>
+                <th><?= $this->Paginator->sort('name') ?></th>
+                <th><?= $this->Paginator->sort('item_quantity') ?></th>
+                <th><?= $this->Paginator->sort('item_price') ?></th>
+                <th><?= $this->Paginator->sort('quantity_threshold') ?></th>
+                <th><?= $this->Paginator->sort('description') ?></th>
+                <th><?= $this->Paginator->sort('category_id') ?></th>
+                <th class="actions"><?= __('Actions') ?></th>
+            </tr>
             </thead>
             <tbody>
-                <?php foreach ($items as $item): ?>
+            <?php foreach ($items as $item): ?>
                 <tr>
                     <td><?= $this->Number->format($item->id) ?></td>
                     <td><?= h($item->name) ?></td>
@@ -30,25 +42,22 @@
                     <td><?= $this->Number->format($item->item_price) ?></td>
                     <td><?= $this->Number->format($item->quantity_threshold) ?></td>
                     <td><?= h($item->description) ?></td>
-                    <td><?= $this->Number->format($item->category_id) ?></td>
+                    <td><?= h($item->category->name) ?></td>
                     <td class="actions">
                         <?= $this->Html->link(__('View'), ['action' => 'view', $item->id]) ?>
                         <?= $this->Html->link(__('Edit'), ['action' => 'edit', $item->id]) ?>
                         <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $item->id], ['confirm' => __('Are you sure you want to delete # {0}?', $item->id)]) ?>
                     </td>
                 </tr>
-                <?php endforeach; ?>
+            <?php endforeach; ?>
             </tbody>
         </table>
     </div>
-    <div class="paginator">
-        <ul class="pagination">
-            <?= $this->Paginator->first('<< ' . __('first')) ?>
-            <?= $this->Paginator->prev('< ' . __('previous')) ?>
-            <?= $this->Paginator->numbers() ?>
-            <?= $this->Paginator->next(__('next') . ' >') ?>
-            <?= $this->Paginator->last(__('last') . ' >>') ?>
-        </ul>
-        <p><?= $this->Paginator->counter(__('Page {{page}} of {{pages}}, showing {{current}} record(s) out of {{count}} total')) ?></p>
-    </div>
+    <script>
+        $(document).ready(function() {
+            $('#dataTable').DataTable();
+        });
+
+    </script>
 </div>
+
