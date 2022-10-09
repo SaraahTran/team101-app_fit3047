996b1170ade41613e41df1338e81be1e56081bb1
Zhenhao <zzhu0019@student.monash.edu>
2022-10-09 15:41:47 +0800

    change 11

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -48,7 +48,7 @@
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
-                <a class="nav-link" href="/">
+                <a class="nav-link" href="<?= $this->Url->build('/') ?>">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
                     <span>Dashboard</span></a>
             </li>
